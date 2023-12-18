fb0ba466be981043c7111b19c952ff089f64a786
621352 <camarenburt>
2023-12-18 09:30:23 -0500

    daf

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
-public class Main {
+import java.util.Scanner;
+public class FavNumbers {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Your favorite int is " + InputHelper.getInt(scan, "Enter your favorite int:") + " and your favorite double is " + InputHelper.getDouble(scan, "Enter your favorite double:"));
     }
 }
